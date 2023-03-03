a19b456e212b91b308d2529e3a656c980a9751b4
Kang SY <ksy5048@kookmin.ac.kr>
2023-03-03 16:28:37 +0900

    login initial commit

---
+++ app/src/main/java/com/example/photocarpet/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.photocarpet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.realphotocarpet.databinding.ActivityEditProfileBinding
+import com.example.realphotocarpet.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLoginKakao.setOnClickListener{
+            val intent = Intent(this,EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
