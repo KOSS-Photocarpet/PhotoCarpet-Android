427367f51a07a0d9a0b8e0a54902c1c8b88c6318
Kang SY <ksy5048@kookmin.ac.kr>
2023-03-08 19:34:26 +0900

    [feat] 로그인 intent 전환

--- app/src/main/java/com/example/photocarpet/LoginActivity.kt
+++ app/src/main/java/com/example/photocarpet/LoginActivity.kt
@@ -3,12 +3,19 @@ package com.example.photocarpet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photocarpet.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnLoginKakao.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
