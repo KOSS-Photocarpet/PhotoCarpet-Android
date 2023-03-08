b36d32d3b63e1279eaa391d5fb17ae37bee6a085
Kang SY <ksy5048@kookmin.ac.kr>
2023-03-08 19:23:52 +0900

    [feat] 프로필 수정 화면 구현

---
+++ app/src/main/java/com/example/photocarpet/EditProfileActivity.kt
@@ -0,0 +1,88 @@
+package com.example.photocarpet
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.photocarpet.databinding.ActivityEditProfileBinding
+import java.io.File
+
+class EditProfileActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditProfileBinding
+    lateinit var filePath: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //갤러리 요청 런처
+        val requestGallerLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        )
+        {
+            try {
+                val calRatio = calculateInSampleSize(
+                    it.data!!.data!!,
+                    resources.getDimensionPixelSize(R.dimen.imgSize),
+                    resources.getDimensionPixelSize(R.dimen.imgSize)
+                )
+                val option = BitmapFactory.Options()
+                option.inSampleSize = calRatio // 이미지 크기 조정
+
+                var inputStream = contentResolver.openInputStream(it.data!!.data!!)
+                val bitmap = BitmapFactory.decodeStream(inputStream, null, option)
+                inputStream = null
+
+                bitmap?.let {
+                    binding.userImageView.setImageBitmap(bitmap)
+                } ?: let {
+                    Log.d("kkang", "bitmap null")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        binding.selectImageButton.setOnClickListener {
+            //gallery app
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            requestGallerLauncher.launch(intent)
+        }
+    }
+    private fun calculateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        try {
+            var inputStream = contentResolver.openInputStream(fileUri)
+
+            //inJustDecodeBounds 값을 true 로 설정한 상태에서 decodeXXX() 를 호출.
+            //로딩 하고자 하는 이미지의 각종 정보가 options 에 설정 된다.
+            BitmapFactory.decodeStream(inputStream, null, options)
+            inputStream!!.close()
+            inputStream = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        //비율 계산........................
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+        //inSampleSize 비율 계산
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
+    }
+}
