9395573b29a33744c504cff877653561e766c3e9
Kang SY <ksy5048@kookmin.ac.kr>
2023-03-12 16:27:46 +0900

    [feat] main home intent 연결

--- app/src/main/java/com/example/photocarpet/MainHomeActivity.kt
+++ app/src/main/java/com/example/photocarpet/MainHomeActivity.kt
@@ -1,11 +1,20 @@
 package com.example.photocarpet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photocarpet.databinding.ActivityMainHomeBinding
 
 class MainHomeActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_home)
+        binding = ActivityMainHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMyPage.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
